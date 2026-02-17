d2eb8c8192f5929978dda2921c2edafdb2f50d64
JohnSang16 <johnsang1970@gmail.com>
2026-02-17 15:38:36 -0500

    working sol using enumerate

--- Prefix/724. Find Pivot Index.py
+++ Prefix/724. Find Pivot Index.py
@@ -1,24 +1,23 @@
+#make leftside be 0 and rightside the sum of the array
+#for every idx subtract the value at that idx from rightsum and check if this equals the leftside
+#if it doesn't add the value to leftside and retry
 class Solution:
     def pivotIndex(self, nums: list[int]) -> int:
-        pivot = 0
-        leftsum = 0 
-        rightsum = 0
-        for i in range(len(nums)):
-            pivot = i
-            while pivot < len(nums)-1:
-                pivot += 1
-                rightsum += nums[pivot] 
-            
-            if i == 0: 
-                pass
-            else:
-                leftsum += nums[i-1]
-            
-            pivot = i
-            if leftsum == rightsum:
-                return pivot
-            rightsum = 0
+        leftside = 0
+        #rightide is the sum of nums
+        rightside = sum(nums)
+        #this stores the index values of nums in idx and the actual values into val using enumerate
+        for idx, val in enumerate(nums):
+            #subtract the val at the index from rightside
+            rightside -= val
+            if leftside == rightside:
+                return idx
+            #add the val at the index to rightside
+            leftside += val
+        #if no pivot is found return -1
         return -1
+
+        
     
 c = Solution()
 test1 = c.pivotIndex([1,7,3,6,5,6])
