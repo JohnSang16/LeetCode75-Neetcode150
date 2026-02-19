774fc1fd12cfc0c0fbf3dd606a02be3069c6b68d
JohnSang16 <johnsang1970@gmail.com>
2026-02-18 23:50:05 -0500

    working sol found

--- Hash Map and Set/2215. Find the Difference of Two Arrays.py
+++ Hash Map and Set/2215. Find the Difference of Two Arrays.py
@@ -1,17 +1,21 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        ans1 = set()
-        ans2 = set()
+        nums1Set = set(nums1)
+        nums2Set = set(nums2)
 
-        d2 = dict(enumerate(nums2))
+        res1 = set()
+        res2 = set()
 
-        for idx, val in enumerate(nums1):
-            while idx <= len(nums1)-1:
-                if val != d2[idx] and idx == len(nums1)-1:
-                    ans1.add(val)
-                if d2[idx] != val and idx == len(nums1)-1:
-                    ans2.add(d2[idx])
-        return list(ans1, ans2)
-        
+        for n in nums1Set:
+            if n not in nums2Set:
+                res1.add(n)
+
+        for n in nums2Set:
+            if n not in nums1Set:
+                res2.add(n)
+
+        return [list(res1), list(res2)]
+    
+
 
         
