2dcc39e2525d33681648edeaadb2c8a62bc747e6
JohnSang16 <johnsang1970@gmail.com>
2026-02-19 23:51:59 -0500

    iterative solution found

--- Hash Map and Set/1207. Unique Number of Occurrences.py
+++ Hash Map and Set/1207. Unique Number of Occurrences.py
@@ -13,6 +13,39 @@
 # Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
 # Output: true
  
+#check if val is unique
+#if it is, create new var for that val as a counter
+#if it is not find that val's counter var and increment
+#at the end check all counters
+#make hash set --> key is unique num in arr, val is counter 
+
 class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        pass
+    def uniqueOccurrences(self, arr: list[int]) -> bool:
+        dictForCount = {}
+
+        for num in arr: 
+            if num not in dictForCount:
+                dictForCount[num] = 1
+            else:
+                dictForCount[num] += 1
+        
+        dictVal = list(dictForCount.values())
+        seen = set()
+
+        for val in dictVal:
+            if val not in seen:
+                seen.add(val)
+            else:
+                pass
+        
+        if len(seen) != len(dictVal):
+            return False
+        else:
+            return True
+
+
+
+c = Solution()
+
+test1 = c.uniqueOccurrences([1,2,2,1,1,3])
+print(test1)
