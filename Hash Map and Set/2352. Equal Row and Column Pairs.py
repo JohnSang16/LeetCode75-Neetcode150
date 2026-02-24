35c7453dc3e81d854ef6a415d136e34b83043763
JohnSang16 <johnsang1970@gmail.com>
2026-02-24 02:16:43 -0500

    working sol for 2352

--- Hash Map and Set/2352. Equal Row and Column Pairs.py
+++ Hash Map and Set/2352. Equal Row and Column Pairs.py
@@ -16,6 +16,31 @@
 # - (Row 2, Column 2): [2,4,2,2]
 # - (Row 3, Column 2): [2,4,2,2]
 
+#we want to compare 2 things: rows and columns
+#we can each row and each col as hash
+#check if hash and rows are equal in values and order
+#if so return the index
+
+
+from collections import defaultdict
 class Solution:
-    def equalPairs(self, grid: List[List[int]]) -> int:
+    def equalPairs(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+        rows = defaultdict(int)
+        pairs = 0 
+        #stores counter for rows into key of the defaultdict rows
+        for row in grid:
+            rows[tuple(row)] += 1 
         
+        #tuple for the columns in grid
+        for col in range(n):
+            column = tuple(grid[r][col] for r in range(n))
+            #if the col is found in the row dict 
+            #store that pair into pairs var
+            if column in rows: 
+                pairs += rows[column]
+        return pairs
+
+c = Solution()
+test1 = c.equalPairs([[3,2,1],[1,7,6],[2,7,7]])
+print(test1)
